0fdb8f880a598aba98592e433b50fe1cf75b047d
Anthony <anthony.tron@etu.univ-amu.fr>
2020-07-04 08:33:30 +0000

    Now properly saves PNG and JPG pictures.

--- resize-gen.jsx
+++ resize-gen.jsx
@@ -1,41 +1,74 @@
-const possibleWidths = [4096, 2048, 1024, 512, 256, 128, 64, 32, 16]
+const possibleWidths = [4096, 2048, 1024, 512, 256, 128, 64, 32, 16];
 
 String.prototype.removeExtension = function() {
-	return this.split('.').slice(0, -1).join('.')
+	return this.split('.').slice(0, -1).join('.');
 }
 
-Array.prototype.filter = function(func) {
-	var newArr = []
+if (!Array.prototype.filter) {
+	Array.prototype.filter = function(func) {
+		var newArr = [];
 
-	for (var i = 0; i < this.length; ++i)
-		if (func(this[i]))
-			newArr.push(this[i])
-		
-	return newArr
+		for (var i = 0; i < this.length; ++i)
+			if (func(this[i]))
+				newArr.push(this[i]);
+			
+		return newArr;
+	}
 }
 
-var saveOptions = new PNGSaveOptions()
-saveOptions.compression = 9
-saveOptions.interlaced = true
+if (!Array.prototype.last){
+    Array.prototype.last = function(){
+        return this[this.length - 1];
+    };
+};
 
-app.preferences.rulerUnits = Units.PIXELS
+File.prototype.extension = function() {
+	return this.name.split('.').last();
+};
 
+function saveOptions(file) {
+	var saveOptions;
+
+	switch (file.extension()) {
+		case 'jpg':
+		case 'jpeg':
+			saveOptions = new JPEGSaveOptions();
+			saveOptions.quality = 10;
+			saveOptions.formatOptions = FormatOptions.STANDARDBASELINE;
+			saveOptions.matte = MatteType.WHITE;
+			break;
+
+		default: // png is default
+			saveOptions = new PNGSaveOptions();
+			saveOptions.compression = 9;
+			saveOptions.interlaced = true;
+			break;
+	}
+	
+	return saveOptions;
+}
+
+
+app.preferences.rulerUnits = Units.PIXELS;
 
 // MAIN >>>
-var file = app.openDialog()[0]
-app.open(file)
+var file = app.openDialog()[0];
+app.open(file);
 
-const ratio = app.activeDocument.width / app.activeDocument.height
+var saveOptions = saveOptions(file);
 
-var widthsToGenerate = possibleWidths.filter(function(x) { return x < app.activeDocument.width })
+const ratio = app.activeDocument.width / app.activeDocument.height;
+
+var widthsToGenerate = possibleWidths.filter(function(x) { return x < app.activeDocument.width; })
 
 for (var i = 0; i < widthsToGenerate.length; ++i) {
-	currentWidth = widthsToGenerate[i]
-	currentHeight = currentWidth / ratio
+	currentWidth = widthsToGenerate[i];
+	currentHeight = Math.round(currentWidth / ratio);
 
-	app.activeDocument.resizeImage(currentWidth, currentHeight)
+	app.activeDocument.resizeImage(currentWidth, currentHeight);
 
-	app.activeDocument.saveAs(new File(file.path + '\\' + file.name.removeExtension() + '-' + currentWidth + 'px-' + currentHeight + 'px.png'), saveOptions)
+	app.activeDocument.saveAs(
+		new File(file.path + '\\' + file.name.removeExtension() + '-' + currentWidth + 'px-' + currentHeight + 'px'), saveOptions);
 }
 
-alert("Les icônes ont bien été générés dans le répertoire " + file.path + '.')
+alert("Les images ont bien été générées dans le répertoire " + file.path + '.');
